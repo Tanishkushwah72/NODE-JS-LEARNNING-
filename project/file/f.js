e4d754fb487c1f41e0b88f16337a36339f9b6385
Tanishkushwah72 <tannub068@gmail.com>
2026-07-12 13:30:44 +0530

    Complete Node.js learning repository with projects and README

--- project/file/f.js
+++ project/file/f.js
@@ -1,21 +1,21 @@
-// import readline from "readline";
-// import fs from "fs";
-// const rl = readline.createInterface({
-//     input:process.stdin,
-//     output:process.stdout
-// })
-// const filecreate=()=>{
-// rl.question("enter your file name ",(filename)=>{
-// rl.question("enter your file content",(content)=>{ 
-// fs.writeFile(`${filename}.txt`,content ,(err)=>{
-// if(err){
-//     console.log(`you have a error plese try again ,${err}`)
-// }else {
-//     console.log( `file ${filename} is successfully created`);
-// }
-// rl.close();
-// })
-// })
-// })
-// }
-// filecreate();
+import readline from "readline";
+import fs from "fs";
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+const filecreate=()=>{
+rl.question("enter your file name ",(filename)=>{
+rl.question("enter your file content",(content)=>{ 
+fs.writeFile(`${filename}.txt`,content ,(err)=>{
+if(err){
+    console.log(`you have a error plese try again ,${err}`)
+}else {
+    console.log( `file ${filename} is successfully created`);
+}
+rl.close();
+})
+})
+})
+}
+filecreate();
